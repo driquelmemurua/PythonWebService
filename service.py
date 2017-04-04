a039bb9c8b7a36abdd5509b684a939605e57dc79
driquelmemurua <maximiliano.quevedo@mail.udp.cl>
2017-04-04 12:43:21 -0300

    validacion login en proceso

--- service.py
+++ service.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
+def validate_login(form):
+	success = False
+
+	return success
 
 @app.route('/')
 def index():
@@ -8,9 +13,25 @@ def index():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
 	if request.method == 'POST':
-		return 'Hello, World'
+		if validate_login(request.form):
+			return redirect(url_for('chat'))
+		else:
+			return redirect(url_for('bad_login'), code=400)
+
 	else: 
 		return render_template('login.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
+
+@app.errorhandler(400)
+def bad_login(error):
+    return render_template('login.html'), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
