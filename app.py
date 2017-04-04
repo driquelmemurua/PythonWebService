6d028cd3a6998425605a8615fb620ae93dddb095
driquelmemurua <maximiliano.quevedo@mail.udp.cl>
2017-04-04 14:29:34 -0300

    modelos modificados

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, flash, url_for, redirect, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,29 +7,29 @@ app.config['SECRET_KEY'] = "random string"
 
 db = SQLAlchemy(app)
 
-class usuario(db.Model):
+class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
+    user = db.Column(db.String(100))
+    password = db.Column(db.String(100))
 
-def __init__(self, first_name, last_name):
-   self.first_name = first_name
-   self.last_name = last_name
+def __init__(self, user, password):
+   self.user = user
+   self.password = password
 
-class mensaje(db.Model):
+class Mensaje(db.Model):
     __tablename__ = 'mensajes'
     id = db.Column(db.Integer, primary_key=True)
-    chat = db.Column(db.String(100))
+    contenido = db.Column(db.String(100))
    
 def __init__(self, chat):
-   self.chat = chat
+   self.contenido = contenido
 
-class envio(db.Model):
+class Envio(db.Model):
     __tablename__ = 'envios'
     id = db.Column(db.Integer, primary_key=True)
-    usuario_id = db.Column(db.String(100))
-    mensaje_id = db.Column(db.String(100))
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    mensaje_id = db.Column(db.Integer, db.ForeignKey('mensajes.id'))
    
 def __init__(self, usuario_id, mensaje_id):
    self.usuario_id = usuario_id
